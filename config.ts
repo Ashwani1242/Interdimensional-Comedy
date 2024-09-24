af2611739612a8d6bd5ecd56d14893ec456badd8
Ashwani1242 <ashwanigupta1242@gmail.com>
2024-09-24 23:32:12 +0530

    Changes Committed on 2024-09-24, 23:32

--- config.ts
+++ config.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'https://interdimensional-comedy-backend.onrender.com/'
+// export const BASE_URL = 'https://interdimensional-comedy-backend.onrender.com/'
+export const BASE_URL = import.meta.env.BASE_URL
