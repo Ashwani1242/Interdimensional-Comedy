cf32fd74dec87fc221552a20df50466446cf461f
Ashwani1242 <ashwanigupta1242@gmail.com>
2024-10-09 21:16:12 +0530

    Changes Committed on 2024-10-09, 21:16

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 // export const BASE_URL = import.meta.env.BASE_URL
-export const BASE_URL = 'https://6f52-2409-40e3-1003-dda6-68b4-ac44-5d8b-3ee2.ngrok-free.app'
+export const BASE_URL = 'https://9ea4-2409-40e3-1000-fea3-bd54-afd6-7595-948.ngrok-free.app'
 // export const BASE_URL = 'http://localhost:8000'
 export const LocalHost = 'http://localhost:8000'
